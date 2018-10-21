a0ca17296f9d3ad8e507caefaad74c539fa97670
Michael Ho <michael.ho@bluespurs.com>
2018-10-21 15:49:15 -0300

    updates

--- src/main/java/com/automation/pageObjects/BasePage.java
+++ src/main/java/com/automation/pageObjects/BasePage.java
@@ -1,9 +1,19 @@
 package com.automation.pageObjects;
 
+import org.openqa.selenium.WebElement;
 import com.automation.stepDefinitions.Hooks;
 import com.automation.utils.SeleniumUtils;
 
 public class BasePage {
+
+    /*
+    * Common WebElements that should be available to all Page Objects
+    * These WebElements are not always ready so they should be returned per request.
+    */
+    public static WebElement account() {return SeleniumUtils.getWebElement("css", "div.header_user_info > a.account");}
+    public static WebElement logout() {return SeleniumUtils.getWebElement("css", "div.header_user_info > a.logout");}
+    public static WebElement contact() {return SeleniumUtils.getWebElement("id", "contact-link");}
+    public static WebElement login() {return SeleniumUtils.getWebElement("css", "div.header_user_info > a.login");}
 
     public static void navigateToPage(String expectedUrl) {
         SeleniumUtils.navigateToURL(expectedUrl);
--- src/main/java/com/automation/stepDefinitions/LoginStepDefinition.java
+++ src/main/java/com/automation/stepDefinitions/LoginStepDefinition.java
@@ -1,6 +1,7 @@
 package com.automation.stepDefinitions;
 
 import com.automation.pageObjects.LoginPage;
+import com.automation.utils.SeleniumUtils;
 import cucumber.api.java.en.Given;
 
 public class LoginStepDefinition {
@@ -8,6 +9,7 @@ public class LoginStepDefinition {
     @Given("I have logged into Automation Practice")
     public void loginAutomationPractice() {
         LoginPage.navigateToPage(Hooks.url);
+        SeleniumUtils.clickElement(LoginPage.login());
         LoginPage.login(Hooks.email, Hooks.password);
     }
 }
